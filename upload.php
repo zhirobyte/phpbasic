36e8582f50137898c83500fe2d15912f606e98c6
Zhirobyte <faizhanafi90@gmail.com>
2021-04-12 15:01:28 +0700

    menambahkan index dan upload php

---
+++ upload.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <?php
+            <form enctype="multipart/form-data" method="post" action="index.php?p?=uploadpost">
+            File yang diupload = <input type="file" name="fupload"> <br>
+            <br> <input type="submit" value="Upload!">
+            </form>
+
+
+    ?>    
+</body>
+</html>
